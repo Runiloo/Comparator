cf56b2b4e32c278ee793a2afa5cfdc48172c5f85
Runiloo <ldi199119@gmail.com>
2017-10-31 18:49:48 +0300

    Done

--- src/main/java/Comparator/App_UI.java
+++ src/main/java/Comparator/App_UI.java
@@ -26,9 +26,9 @@ import java.awt.event.ActionEvent;
 public class App_UI {
 
 	private JFrame frame;
-	private JTextField path1;
-	private JTextField path2;
-	private JTextField path3;
+	private JTextField pathField1;
+	private JTextField pathField2;
+	private JTextField pathField3;
 
 	/**
 	 * Launch the application.
@@ -57,41 +57,44 @@ public class App_UI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		/*Main window */
 		frame = new JFrame();
 		frame.setBounds(100, 100, 584, 323);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		/*Create field for input path for Folder №1*/
+		pathField1 = new JTextField();
+		pathField1.setBounds(12, 50, 433, 22);
+		frame.getContentPane().add(pathField1);
+		pathField1.setColumns(10);
+		/*Create field for input path for Folder №2*/
+		pathField2 = new JTextField();
+		pathField2.setBounds(12, 114, 433, 22);
+		frame.getContentPane().add(pathField2);
+		pathField2.setColumns(10);
+		/*Create field for input path for result folder */
+		pathField3 = new JTextField();
+		pathField3.setBounds(12, 178, 433, 22);
+		frame.getContentPane().add(pathField3);
+		pathField3.setColumns(10);
 		
-		path1 = new JTextField();
-		path1.setBounds(12, 50, 433, 22);
-		frame.getContentPane().add(path1);
-		path1.setColumns(10);
-		
-		path2 = new JTextField();
-		path2.setBounds(12, 114, 433, 22);
-		frame.getContentPane().add(path2);
-		path2.setColumns(10);
-		
-		path3 = new JTextField();
-		path3.setBounds(12, 178, 433, 22);
-		frame.getContentPane().add(path3);
-		path3.setColumns(10);
-		
+		/*UI description of pathField1*/
 		JLabel lblNewLabel = new JLabel("Folder №1:");
 		lblNewLabel.setBounds(12, 29, 71, 16);
 		frame.getContentPane().add(lblNewLabel);
-		
+		/*UI description of pathField2*/
 		JLabel lblNewLabel_1 = new JLabel("Folder №2:");
 		lblNewLabel_1.setBounds(12, 85, 71, 16);
 		frame.getContentPane().add(lblNewLabel_1);
-		
+		/*UI description of pathField3*/
 		JLabel lblNewLabel_2 = new JLabel("Folder with results:");
 		lblNewLabel_2.setBounds(12, 149, 150, 16);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		
-		
+		/*
+		 * Create button for choosing folder №1 path  */
 		JButton explorerBtn1 = new JButton("Select");
 		explorerBtn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -99,7 +102,7 @@ public class App_UI {
 		        dialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		        int ret = dialog.showDialog(null, "OK");
 		        if (ret == JFileChooser.APPROVE_OPTION) {
-		            path1.setText(dialog.getSelectedFile().getAbsolutePath());		            
+		            pathField1.setText(dialog.getSelectedFile().getAbsolutePath());		            
 		        }
 			}
 		});
@@ -107,7 +110,8 @@ public class App_UI {
 		frame.getContentPane().add(explorerBtn1);
 		
 		
-		
+		/*
+		 * Create button for choosing folder №2 path  */
 		JButton explorerBtn2 = new JButton("Select");
 		explorerBtn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -115,7 +119,7 @@ public class App_UI {
 		        dialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		        int ret = dialog.showDialog(null, "OK");
 		        if (ret == JFileChooser.APPROVE_OPTION) {
-		            path2.setText(dialog.getSelectedFile().getAbsolutePath());		            
+		            pathField2.setText(dialog.getSelectedFile().getAbsolutePath());		            
 		        }				
 			}
 		});
@@ -124,7 +128,8 @@ public class App_UI {
 		
 		
 		
-		
+		/*
+		 * Create button for choosing folder №3 path  */
 		JButton explorerBtn3 = new JButton("Select");
 		explorerBtn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -132,7 +137,7 @@ public class App_UI {
 		        dialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		        int ret = dialog.showDialog(null, "OK");
 		        if (ret == JFileChooser.APPROVE_OPTION) {
-		            path3.setText(dialog.getSelectedFile().getAbsolutePath());		            
+		            pathField3.setText(dialog.getSelectedFile().getAbsolutePath());		            
 		        }				
 			}
 		});
@@ -140,32 +145,38 @@ public class App_UI {
 		frame.getContentPane().add(explorerBtn3);
 		
 		
-		
+		/*Create button for starting comparing. Check to approving folders path. */
 		JButton compareBtn = new JButton("Compare");
 		compareBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				boolean isOK = true;				
-				File directory1 = new File(path1.getText());					
+				File directory1 = new File(pathField1.getText());					
 				if(!directory1.exists()) {
 					JOptionPane.showMessageDialog(null, "Folder №1 is incorrect");
 					isOK = false;
 				}		
 				
-				File directory2 = new File(path2.getText());					
+				File directory2 = new File(pathField2.getText());					
 				if(!directory2.exists()) {
 					JOptionPane.showMessageDialog(null, "Folder №2 is incorrect");
 					isOK = false;
 				}
 				
-				File directory3 = new File(path3.getText());					
-				if(!directory3.exists()) {
+				File directory3 = new File(pathField3.getText());
+				
+				if(!directory3.getParentFile().exists()) {					
 					JOptionPane.showMessageDialog(null, "Result folder is incorrect");
+					isOK = false;
+				}
+				if(directory3.toString().equals(directory2.toString())||directory3.toString().equals(directory1.toString())) {
+					JOptionPane.showMessageDialog(null, "Result folder must be not \"Folder №1\" or \"Folder №2\"");
 					isOK = false;
 				}
 				
 				if (isOK) {
+					directory3.mkdir();
 					try {
-						compare(directory1, directory2, path3.getText());
+						compare(directory1, directory2, pathField3.getText());
 					} catch (IOException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -181,84 +192,88 @@ public class App_UI {
 	/* 
 	 * Compare files and push it in result folder
 	 * **/
-	public void compare (File fold1, File fold2, String foldResult) throws IOException {
-		ArrayList <File> filesArr = new ArrayList <File>();
-		Path destDir = Paths.get(foldResult);
-		File[] files1 = fold1.listFiles();
-		File[] files2 = fold2.listFiles();
-		
-		String foldNum1 = "(1)";
-		String foldNum2 = "(2)";		
+	public void compare (File folder1, File folder2, String foldResult) throws IOException {
+		/*Create changers for files list*/
+		ArrayList <File> filesArray = new ArrayList <File>();
 		Stack <File> filesStack = new Stack<File>();
 		
-		if (files2.length>files1.length) {
-			foldNum1 = "(2)";
-			foldNum2 = "(1)";
-			for(File file2: files2) {
-				filesStack.push(file2);
+		Path destinationFolder = Paths.get(foldResult);
+		
+		File[] files1 = folder1.listFiles();
+		File[] files2 = folder2.listFiles();
+		
+		String folderNum1 = "(1)";
+		String folderNum2 = "(2)";		
+		
+		
+		/*if (files2.length>files1.length) {
+			folderNum1 = "(2)";
+			folderNum2 = "(1)";
+			for(File file: files2) {
+				filesStack.push(file);
 				}
 			files2 = files1;
 		}
-		else {
-			for(File file1: files1) {
-				filesStack.push(file1);
+		else {*/
+			for(File file: files1) {
+				filesStack.push(file);
 				}
-		}
+		/*}*/
 		
 		for(File file: files2) {
-			filesArr.add(file);
+			filesArray.add(file);
 		}
 		
-		
+		/*Compare files from two arrays and put files to result folder if they have differences by name, size or changing time*/
 		while (!filesStack.empty()) {
 			File file1 = filesStack.pop();
-			boolean isEqual = false;			
+			boolean isCoincided = false;			
 			long sizeF1 = file1.length();
 			long dateF1 = file1.lastModified();			
 			
-			for(int i =0; i<filesArr.size();i++) {	
-				File file2 = filesArr.get(i);
+			for(int i =0; i<filesArray.size();i++) {	
+				File file2 = filesArray.get(i);
 				if(file1.getName().equals(file2.getName())){
 					if(sizeF1 == file2.length()) {
 						if(dateF1 == file2.lastModified()) {
-							isEqual = true;
-							filesArr.remove(i);
+							isCoincided = true;
+							filesArray.remove(i);
 							}
 						else {
 							if (file2.isDirectory())
-								copyFolder(file2, foldResult, foldNum2);
+								copyFolder(file2, foldResult, folderNum2);
 							else 
-								Files.copy(file2.toPath(), destDir.resolve(foldNum2+file2.getName()), StandardCopyOption.REPLACE_EXISTING);
-								
-								filesArr.remove(i);						
+								Files.copy(file2.toPath(), destinationFolder.resolve(folderNum2+file2.getName()), StandardCopyOption.REPLACE_EXISTING);								
+								filesArray.remove(i);						
 						}
 					}
 					else {	
 						if (file2.isDirectory())
-							copyFolder(file2, foldResult, foldNum2);
+							copyFolder(file2, foldResult, folderNum2);
 						else 
-							Files.copy(file2.toPath(), destDir.resolve(foldNum2+file2.getName()), StandardCopyOption.REPLACE_EXISTING);
-							
-							filesArr.remove(i);						
+							Files.copy(file2.toPath(), destinationFolder.resolve(folderNum2+file2.getName()), StandardCopyOption.REPLACE_EXISTING);							
+							filesArray.remove(i);						
 					}
 				}				
 			}
-			if(isEqual) continue;
+			if(isCoincided) continue;
 			if (file1.isDirectory())
-				copyFolder(file1, foldResult, foldNum1);
+				copyFolder(file1, foldResult, folderNum1);
 			else
-				Files.copy(file1.toPath(), destDir.resolve(foldNum1 + file1.getName()), StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(file1.toPath(), destinationFolder.resolve(folderNum1 + file1.getName()), StandardCopyOption.REPLACE_EXISTING);
 		}			
-		for(File file: filesArr) {
+		
+		/*if in second folder files remain here it put in result folder*/
+		for(File file: filesArray) {
 			if (file.isDirectory())
-				copyFolder(file, foldResult , foldNum2);
+				copyFolder(file, foldResult , folderNum2);
 			else
-				Files.copy(file.toPath(), destDir.resolve(foldNum2 + file.getName()), StandardCopyOption.REPLACE_EXISTING);			
+				Files.copy(file.toPath(), destinationFolder.resolve(folderNum2 + file.getName()), StandardCopyOption.REPLACE_EXISTING);			
 		}
 	}
 	
 	/* 
-	 * recursive copying of folder
+	 * recursive copying for folders and folders in folders
 	 * **/
 	public void copyFolder (File fold, String path, String foldNum) throws IOException {
 		File[] files = fold.listFiles();
